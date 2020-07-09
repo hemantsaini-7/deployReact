b1c8a86b0723b2311e83febfd66d5bac60f1ea3e
Hemant Saini <hsainioff@gmail.com>
2020-07-09 21:41:05 +0530

    Updates

---
+++ precache-manifest.845ab612bc8c636d7745d683f4132bf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb8765794cdb4ea12cfcc3fe9a6f3e0",
+    "url": "/deployReact/index.html"
+  },
+  {
+    "revision": "804eabb470e07f3b07d9",
+    "url": "/deployReact/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "46b68df87c5de8c8be5d",
+    "url": "/deployReact/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "804eabb470e07f3b07d9",
+    "url": "/deployReact/static/js/2.872d5598.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/deployReact/static/js/2.872d5598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46b68df87c5de8c8be5d",
+    "url": "/deployReact/static/js/main.aa5e4dc1.chunk.js"
+  },
+  {
+    "revision": "9fe251e4d70efed6b72c",
+    "url": "/deployReact/static/js/runtime-main.e5ca0258.js"
+  }
+]);
